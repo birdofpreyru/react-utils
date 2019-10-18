5ea3bb78adb2b4f84fd860fce8382a1141337b94
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2019-10-18 21:03:20 +0200

    Fixes and improves NavLink tests

--- __tests__/shared/components/NavLink.jsx
+++
@@ -1,7 +0,0 @@
-import NavLink from 'components/NavLink';
-import React from 'react';
-import { shallowRender } from 'utils/jest';
-
-test('Matches snapshots', () => {
-  shallowRender(<NavLink />);
-});
