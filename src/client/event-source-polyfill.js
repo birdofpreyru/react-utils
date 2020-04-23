dc1fed0a3a242b7d6e04662c5b4b483151eac461
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-04-23 02:23:20 +0200

    v0.20.3: Fixes tests

--- src/client/event-source-polyfill.js
+++ src/client/event-source-polyfill.js
@@ -1,7 +1,11 @@
 /**
  * A simple wrapper for "event-source-polyfill", to install the polyfill
  * when this script is executed.
+ *
+ * IMPORTANT: This is intended for the dev mode only.
  */
+/* global window */
+/* eslint-disable global-require, import/no-extraneous-dependencies */
 
 if (typeof EventSource === 'undefined') {
   window.EventSource = require('event-source-polyfill').EventSourcePolyfill;
