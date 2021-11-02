cd984888f38ff7958ef4cc05176096c29b257165
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2021-11-02 01:12:07 +0100

    [#196] Updates default Stylelint config
    "custom-property-pattern", "keyframes-name-pattern",
    "scss/at-mixin-pattern" rules are re-configured to allow either
    camelCase or kebab-case.

--- config/stylelint/default.js
+++ config/stylelint/default.js
@@ -1,3 +1,10 @@
+const camelCaseRegex = /^[a-z][a-zA-Z0-9]+$/;
+const kebabCase = /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/;
+
+const namePattern = new RegExp(
+  `(${camelCaseRegex.source})|(${kebabCase.source})`,
+);
+
 module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
@@ -12,6 +19,9 @@ module.exports = {
         'use',
       ],
     }],
+    'custom-property-pattern': namePattern,
+    'keyframes-name-pattern': namePattern,
+    'scss/at-mixin-pattern': namePattern,
     'selector-class-pattern': null,
     'selector-pseudo-class-no-unknown': [
       true, { ignorePseudoClasses: ['global'] },
