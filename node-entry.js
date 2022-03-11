ad9bfd9e592c195e6dd02f3f165729e4b70ad592
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2022-03-11 16:20:10 +0100

    [#228] v1.15.5: Fixes for react-native compatibility

--- node-entry.js
+++ node-entry.js
@@ -1,5 +1,10 @@
-const build = process.env.NODE_ENV === 'production'
-  ? 'production' : 'development';
+// This permits to load the library in react-native, or other environment that
+// does not have __dirname global. It probably breaks a few node-specific lib
+// pieces, but that does not seem to prevent more relevant parts from working.
+// eslint-disable-next-line no-underscore-dangle
+if (typeof __dirname === 'undefined') global.__dirname = '';
 
-// eslint-disable-next-line import/no-dynamic-require
-module.exports = require(`./build/${build}`);
+// Again, for react-native, and maybe some other environments, we need
+// to have no dynamic requires below (i.e. can't do require(`./build/${env}`)).
+module.exports = process.env.NODE_ENV === 'production'
+  ? require('./build/production') : require('./build/development');
