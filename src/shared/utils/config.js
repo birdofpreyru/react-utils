d55c3b2810d1dc137283c6128972d9cb9ba73996
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2022-03-11 18:10:49 +0100

    v1.15.6: [#229] "config" module fallback to empty object

--- src/shared/utils/config.js
+++ src/shared/utils/config.js
@@ -3,5 +3,5 @@
 import { requireWeak } from './webpack';
 
 /* eslint-disable global-require */
-export default requireWeak('config') || window.CONFIG;
+export default requireWeak('config') || window.CONFIG || {};
 /* eslint-enable global-require */
