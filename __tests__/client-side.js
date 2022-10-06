7d37ae23c5c46eeb573dc9fb3822806078295cd9
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2022-10-06 20:39:33 +0200

    Updates some dependencies and downgrades axios to 0.27.2

--- __tests__/client-side.js
+++ __tests__/client-side.js
@@ -5,12 +5,7 @@ import { mockClientSide } from 'utils/jest';
 
 mockdate.set('2020-04-19Z');
 
-// TODO: This was disabled after upgrade of axios to 1.0.0, as it uses not
-// compiled ES imports, and thus requires client side to use webpack build
-// environment. However, current implementation of E2eSsrEnv has misc issues
-// to make it work: SASS fails if we can't set correct context needed to resolve
-// SCSS imports.
-test.skip('Export at client side', () => {
+test('Export at client side', () => {
   mockClientSide();
   const lib = require('../src');
   expect(lib).toMatchSnapshot();
