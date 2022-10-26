76a46d0479959efbb14a788a60d0b2e21300750c
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2022-10-26 12:11:35 +0200

    WIP: [#265] time.useCurrent() hook

---
+++ __tests__/shared/utils/time/__assets__/index.js
@@ -0,0 +1,6 @@
+// This is Scene for testing useCurrent() hook.
+
+import client from 'client';
+import Scene from './Scene';
+
+client(Scene);
--- src/shared/utils/time.js
+++ src/shared/utils/time.js
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs';
 import { noop } from 'lodash';
+import { useEffect } from 'react';
+
+import { useGlobalState } from '@dr.pogodin/react-global-state';
 
 import { Barrier } from './Barrier';
 
@@ -92,8 +95,8 @@ export async function timer(timeout) {
  * is also passed and used in the first client side render, and then updated
  * with a finite precision to avoid infinite re-rendering loops.
  * @param {object} [options] Optional settings.
- * @param {string} [options.path="currentTime"] Global state path to keep
- *  the current time value.
+ * @param {string} [options.globalStatePath="currentTime"] Global state path
+ *  to keep the current time value.
  * @param {number} [options.precision= 5 * time.SEC_MS] Current time precision.
  *  The hook won't update the current time stored in the global state unless it
  *  is different from Date.now() result by this number (in milliseconds).
@@ -104,11 +107,30 @@ export async function timer(timeout) {
  *  the global state with the period equal to the `precision`.
  * @return {number} Unix timestamp in milliseconds.
  */
-export function useCurrent(options) {
-
+export function useCurrent({
+  autorefresh = false,
+  globalStatePath = 'currentTime',
+  precision = 5 * dayjs.SEC_MS,
+} = {}) {
+  const [now, setter] = useGlobalState(globalStatePath, Date.now);
+  useEffect(() => {
+    let timerId;
+    const update = () => {
+      setter((old) => {
+        const neu = Date.now();
+        return Math.abs(neu - old) > precision ? neu : old;
+      });
+      if (autorefresh) timerId = setTimeout(update, precision);
+    };
+    update();
+    return () => {
+      if (timerId) clearTimeout(timerId);
+    };
+  }, [autorefresh, precision, setter]);
+  return now;
 }
 
 dayjs.timer = timer;
-dayjs.useNow = useCurrent;
+dayjs.useCurrent = useCurrent;
 
 export default dayjs;
