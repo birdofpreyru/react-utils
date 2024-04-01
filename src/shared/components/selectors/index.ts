5a380f92486249042e3d17960c345bd6bfdbe485
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2024-04-02 00:18:54 +0200

    [#332] Exports type and validator for dropdown/switch option

--- src/shared/components/selectors/index.ts
+++ src/shared/components/selectors/index.ts
@@ -1,3 +1,10 @@
 export { default as CustomDropdown } from './CustomDropdown';
 export { default as Dropdown } from './NativeDropdown';
 export { default as Switch } from './Switch';
+
+export {
+  type OptionT,
+  type OptionsT,
+  optionValidator,
+  optionsValidator,
+} from './common';
