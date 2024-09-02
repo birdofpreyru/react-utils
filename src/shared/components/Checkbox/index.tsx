942f7bd97bf6e74d1444801681c865e34cf8de60
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2024-09-02 20:20:38 +0200

    [#347] Checkbox: adds "disabled" prop

--- src/shared/components/Checkbox/index.tsx
+++ src/shared/components/Checkbox/index.tsx
@@ -4,29 +4,41 @@ import defaultTheme from './theme.scss';
 
 type PropT<ValueT> = {
   checked?: ValueT;
+  disabled?: boolean;
   label?: React.ReactNode;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
   testId?: string;
-  theme: Theme<'checkbox' | 'container' | 'indeterminate' | 'label'>;
+  theme: Theme<
+  | 'checkbox'
+  | 'container'
+  | 'disabled'
+  | 'indeterminate'
+  | 'label'
+  >;
 };
 
 const Checkbox = <ValueT extends boolean | 'indeterminate' = boolean>({
   checked,
+  disabled,
   label,
   onChange,
   testId,
   theme,
 }: PropT<ValueT>) => {
+  let containerClassName = theme.container;
+  if (disabled) containerClassName += ` ${theme.disabled}`;
+
   let checkboxClassName = theme.checkbox;
   if (checked === 'indeterminate') checkboxClassName += ` ${theme.indeterminate}`;
 
   return (
-    <div className={theme.container}>
+    <div className={containerClassName}>
       { label === undefined ? null : <div className={theme.label}>{label}</div> }
       <input
         checked={checked === true}
         className={checkboxClassName}
         data-testid={process.env.NODE_ENV === 'production' ? undefined : testId}
+        disabled={disabled}
         onChange={onChange}
         onClick={(e) => e.stopPropagation()}
         type="checkbox"
