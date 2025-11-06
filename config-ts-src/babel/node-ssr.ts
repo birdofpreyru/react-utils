210713cb7d93d564a37ec3428fdd07af3f6a57d5
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-11-07 00:42:08 +0100

    v1.47.0-alpha.7; Drops Node 20 support

--- config-ts-src/babel/node-ssr.ts
+++ config-ts-src/babel/node-ssr.ts
@@ -44,7 +44,7 @@ function newBase(babel: BabelCompilerI, options: OptionsT = {}) {
       // Jest relies on babel.config.js, which transpiles everything
       // into CJS).
       modules: 'cjs',
-      targets: 'current node',
+      targets: 'maintained node versions',
       ...options,
     },
   );
